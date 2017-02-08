f5f1c48163156a8c4372ce96e920d66853704c24
jondaveyan <eduard@rllinsure.com>
2017-02-08 18:55:21 +0400

    auth

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,12 +3,15 @@
     <h1>Պահեստի շարժ</h1>
 
     <div id="body">
-        <a href="<?php echo base_url('products'); ?>"><button class="btn btn-default">Ապրանքներ</button></a>
-        <a href="<?php echo base_url('clients'); ?>"><button class="btn btn-default">Կլիենտներ</button></a>
-        <a href="<?php echo base_url('orders'); ?>"><button class="btn btn-default">Գործարքներ</button></a>
-        <a href="<?php echo base_url('payment'); ?>"><button class="btn btn-default">Վճարում</button></a>
-        <a href="<?php echo base_url('giveback'); ?>"><button class="btn btn-default">Ապրանքի վերադարձ</button></a>
+        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('products'); ?>"><button class="btn btn-default">Ապրանքներ</button></a><?php } ?>
+        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('clients'); ?>"><button class="btn btn-default">Կլիենտներ</button></a><?php } ?>
+        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('orders'); ?>"><button class="btn btn-default">Գործարքներ</button></a><?php } ?>
+        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('payment'); ?>"><button class="btn btn-default">Վճարում</button></a><?php } ?>
+        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('giveback'); ?>"><button class="btn btn-default">Ապրանքի վերադարձ</button></a><?php } ?>
         <a href="<?php echo base_url('dashboard'); ?>"><button class="btn btn-info">Աղյուսակ</button></a>
+    </div><br>
+    <div>
+        <a href="<?php echo base_url('welcome/logout'); ?>"><button class="btn btn-danger">Logout</button></a>
     </div>
 </div>
 <?php $this->load->view('footer'); ?>
