72cfc29cb829cfe3d40e02231cbca91c833b8b7d
jondaveyan <jon.daveyan@gmail.com>
2017-08-31 23:10:52 +0400

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,6 @@ class Welcome extends CI_Controller {
      */
     function __construct() {
         parent::__construct();
-        $this->load->database();
     }
 
     public function index()
@@ -30,53 +29,47 @@ class Welcome extends CI_Controller {
         $query = $this->db->get('products');
         $products = $query->result();
         $data = array('clients' => $clients, 'products' => $products);*/
-        $this->db->select('logged_in');
-        $this->db->where('id', 1);
-        $query = $this->db->get('sess');
-        $res = $query->result()[0]->logged_in;
-        if($res != 'logout')
+        $this->load->view('login');
+    }
+
+    public function login($user_data = null)
+    {
+        if($user_data)
         {
-            $data = array('user' => $res);
+            $username = $user_data;
+            $data = array('username' => $username);
             $this->load->view('welcome', $data);
         }
         else
         {
-            $this->load->view('login');
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $this->db->select();
+            $this->db->from('users');
+            $this->db->where('username', $username);
+            $this->db->where('password', md5($password));
+            $query = $this->db->get();
+            if($query->num_rows() > 0)
+            {
+                $data = array('username' => $username);
+                $this->load->view('welcome', $data);
+            }
+            else
+            {
+                redirect('welcome');
+            }
         }
-    }
-
-    public function login()
-    {
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        $this->db->select();
-        $this->db->from('users');
-        $this->db->where('username', $username);
-        $this->db->where('password', md5($password));
-        $query = $this->db->get();
-        if($query->num_rows() > 0)
-        {
-            $data = array('logged_in' => $username);
-            $this->db->where('id', 1);
-            $this->db->update('sess', $data);
-        }
-        redirect('welcome');
     }
 
     public function logout()
     {
-        $data = array('logged_in' => 'logout');
-        $this->db->where('id', 1);
-        $this->db->update('sess', $data);
+        $this->session->unset_userdata('logged_in');
+        $this->session->unset_userdata('username');
         redirect('welcome');
     }
 
     public function check_user()
     {
-        $this->db->select('logged_in');
-        $this->db->where('id', 1);
-        $query = $this->db->get('sess');
-        $res = $query->result()[0]->logged_in;
-        echo json_encode(array('res' => $res));
+        echo json_encode(array('res' => $this->session->userdata('username')));
     }
 }
