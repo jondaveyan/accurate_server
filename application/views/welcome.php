fb3e047cde022ed46b2c7fbb55ff1d94a13645b5
jondaveyan <jon.daveyan@gmail.com>
2017-08-31 05:10:30 +0400

    Update welcome.php

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,11 +3,11 @@
     <h1>Պահեստի շարժ</h1>
 
     <div id="body">
-        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('products'); ?>"><button class="btn btn-default">Ապրանքներ</button></a><?php } ?>
-        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('clients'); ?>"><button class="btn btn-default">Կլիենտներ</button></a><?php } ?>
-        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('orders'); ?>"><button class="btn btn-default">Գործարքներ</button></a><?php } ?>
-        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('payment'); ?>"><button class="btn btn-default">Վճարում</button></a><?php } ?>
-        <?php if($this->session->userdata('username') == 'admin'){ ?><a href="<?php echo base_url('giveback'); ?>"><button class="btn btn-default">Ապրանքի վերադարձ</button></a><?php } ?>
+        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('products'); ?>"><button class="btn btn-default">Ապրանքներ</button></a><?php } ?>
+        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('clients'); ?>"><button class="btn btn-default">Կլիենտներ</button></a><?php } ?>
+        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('orders'); ?>"><button class="btn btn-default">Գործարքներ</button></a><?php } ?>
+        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('payment'); ?>"><button class="btn btn-default">Վճարում</button></a><?php } ?>
+        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('giveback'); ?>"><button class="btn btn-default">Ապրանքի վերադարձ</button></a><?php } ?>
         <a href="<?php echo base_url('dashboard'); ?>"><button class="btn btn-info">Աղյուսակ</button></a>
     </div><br>
     <div>
