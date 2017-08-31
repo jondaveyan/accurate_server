d94fb6d389d9929fe4d393f6a1de8969133b54fa
jondaveyan <jon.daveyan@gmail.com>
2017-08-31 23:12:52 +0400

    Update welcome.php

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,15 +3,20 @@
     <h1>Պահեստի շարժ</h1>
 
     <div id="body">
-        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('products'); ?>"><button class="btn btn-default">Ապրանքներ</button></a><?php } ?>
-        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('clients'); ?>"><button class="btn btn-default">Կլիենտներ</button></a><?php } ?>
-        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('orders'); ?>"><button class="btn btn-default">Գործարքներ</button></a><?php } ?>
-        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('payment'); ?>"><button class="btn btn-default">Վճարում</button></a><?php } ?>
-        <?php if($user == 'admin'){ ?><a href="<?php echo base_url('giveback'); ?>"><button class="btn btn-default">Ապրանքի վերադարձ</button></a><?php } ?>
+        <?php if($username == 'admin'){ ?><a href="<?php echo base_url('products'); ?>"><button class="btn btn-default">Ապրանքներ</button></a><?php } ?>
+        <?php if($username == 'admin'){ ?><a href="<?php echo base_url('clients'); ?>"><button class="btn btn-default">Կլիենտներ</button></a><?php } ?>
+        <?php if($username == 'admin'){ ?><a href="<?php echo base_url('orders'); ?>"><button class="btn btn-default">Գործարքներ</button></a><?php } ?>
+        <?php if($username == 'admin'){ ?><a href="<?php echo base_url('payment'); ?>"><button class="btn btn-default">Վճարում</button></a><?php } ?>
+        <?php if($username == 'admin'){ ?><a href="<?php echo base_url('giveback'); ?>"><button class="btn btn-default">Ապրանքի վերադարձ</button></a><?php } ?>
         <a href="<?php echo base_url('dashboard'); ?>"><button class="btn btn-info">Աղյուսակ</button></a>
     </div><br>
     <div>
-        <a href="<?php echo base_url('welcome/logout'); ?>"><button class="btn btn-danger">Logout</button></a>
+        <button id="logoutBtn" class="btn btn-danger">Logout</button>
     </div>
 </div>
 <?php $this->load->view('footer'); ?>
+<script>
+    $(document).ready(function(){
+        localStorage.setItem('username', '<?php echo $username; ?>');
+    })
+</script>
