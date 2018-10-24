d8e90c0a51ce56a705a6f7d9141a4a909c114484
jondaveyan <jon.daveyan@gmail.com>
2018-10-24 08:31:18 +0400

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -6,6 +6,7 @@
         <label for="dbselect">Բազա</label>
         <select id="dbselect" name="database">
             <option value="default">Նորմալ</option>
+            <option value="old2">Արխիվ 2</option>
             <option value="old">Արխիվ</option>
         </select>
         Username:
