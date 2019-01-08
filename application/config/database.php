a606444bc63b7f4d86236e150dc3250ad9a47adb
jondaveyan <jon.daveyan@gmail.com>
2019-01-08 14:27:55 +0400

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -141,3 +141,25 @@ $db['old2'] = array(
     'failover' => array(),
     'save_queries' => TRUE
 );
+
+$db['old3'] = array(
+    'dsn'	=> '',
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'database' => 'accurate_old3',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
